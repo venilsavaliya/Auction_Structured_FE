2f9bdffd9a837fb8c61a142647bc4eee662432b7
venilsavaliya <tatva.pce175@outlook.com>
2025-09-01 14:39:11 +0530

    Add Nullable Image In Team Schema

--- src/Schemas/TeamSchema.ts
+++ src/Schemas/TeamSchema.ts
@@ -3,5 +3,11 @@ import type { TeamFormInput } from "../Models/FormInterfaces/TeamFormInput";
 export const TeamSchema:yup.ObjectSchema<TeamFormInput> = yup.object().shape({
     id:yup.number().optional(),
   name: yup.string().required("Team name is required"),
-  image: yup.mixed(), 
+  image: yup
+  .mixed<File>()
+  .nullable()
+  .test("fileType", "Invalid file", (value) => {
+    return value === null || value instanceof File;
+  }),
+ 
 });
